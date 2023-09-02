347d3006f6dd7d62a787eb47aa39da2826a8574e
takeishi-g <89860661+takeishi-g@users.noreply.github.com>
2023-09-02 21:58:39 +0900

    ビューの使い方について

---
+++ practice-laravel/laravel_test_app/resources/views/post.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>{{ $id }}番目の記事です</title>
+</head>
+<body>
+  <h1>{{ $id }}番目の記事です</h1>
+  <p>日付: {{ date('Y-m-d') }}</p>
+</body>
+</html>
