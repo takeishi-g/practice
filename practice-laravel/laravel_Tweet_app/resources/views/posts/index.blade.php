0576eba7674a5c31a6f3a5ce5f2621f4f55b02a0
takeishi-g <89860661+takeishi-g@users.noreply.github.com>
2023-09-10 23:53:28 +0900

    bootstrap_build_完了

--- practice-laravel/laravel_Tweet_app/resources/views/posts/index.blade.php
+++ practice-laravel/laravel_Tweet_app/resources/views/posts/index.blade.php
@@ -4,6 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>投稿一覧</title>
+
+  @vite(['resources/js/app.js'])
 </head>
 <body>
   <header>
