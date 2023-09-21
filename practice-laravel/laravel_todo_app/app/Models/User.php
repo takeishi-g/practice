8b7e6306a302d40d499765fe7e16170c1613877e
takeishi-g <89860661+takeishi-g@users.noreply.github.com>
2023-09-21 22:32:43 +0900

    テーブルリレーション作成

--- practice-laravel/laravel_todo_app/app/Models/User.php
+++ practice-laravel/laravel_todo_app/app/Models/User.php
@@ -46,4 +46,8 @@ class User extends Authenticatable
     public function goals() {
         return $this->hasMany(Goal::class);
     }
+
+    public function todos() {
+        return $this->hasMany(Todo::class);
+    }
 }
